d903477329e162d26bd12ca1b21c0e71908b9845
joyfulmagician <joyfulmagician@gmail.com>
2024-10-27 09:59:41 -0500

    newly updated

--- scraping.py
+++ scraping.py
@@ -41,12 +41,13 @@ if response.status_code == 200:
         li_elements = ul_element[0].find_all('li', recursive=False)
         
         # Loop through each <li> element
-        # for i in range(1, len(li_elements)):
-        for i in range(5, len(li_elements)):
+        for i in range(1, len(li_elements)):
+        # for i in range(3, 4):
             try:
                 brand_element = li_elements[i].find('a', recursive=False)
                 brand_name = brand_element.text.strip()
                 div_element = li_elements[i].find('div', {"class": "col-1 firstcolumn lastcolumn"})
+                m =0
                 
                 if div_element:
                     a_elements = div_element.select('ul > li > a')
@@ -61,68 +62,85 @@ if response.status_code == 200:
                     category_page = requests.get(href_value)
                     category_soup = BeautifulSoup(category_page.text, 'html.parser')
                     product_listing_element = category_soup.find('div', {"class": "pagination-container cm-pagination-container"})
-                    div_elements = product_listing_element.find_all('div', {"class": "prices-container clearfix"})
-                    
-                    for element in div_elements:
-                        child_a_element = element.find('a', recursive=False)
-                        product_url = child_a_element['href']
-                        product_page = requests.get(product_url)
-                        product_soup = BeautifulSoup(product_page.text, 'html.parser')
-                        options_list = []
-                        
-                        option_div_elements = product_soup.find_all('div', {"class": "control-group product-list-field clearfix"})
-                        if option_div_elements:
-                            for k in range(0, len(option_div_elements)):
-                                select_element = option_div_elements[k].find('select', recursive=False)
-                                if select_element:  # Check if the select element exists
-                                    child_option_elements = select_element.find_all('option', recursive=False)
-                                    for option_element in child_option_elements:
-                                        option_element_text = option_element.text.strip()
-                                        options_list.append(option_element_text)
-                        else:
-                            # Set options_list to None if no matching div elements are found
-                            options_list = None
-                        
-                        # Collect image, name, and price data
-                        image_a_element = product_soup.find('a', {"class": "cm-image-previewer"})
-                        product_image_url = image_a_element['href'] if image_a_element and image_a_element.has_attr('href') else None
-                        product_name_div = product_soup.find('div', {"class": "breadcrumbs clearfix"})
-                        product_name_span = product_name_div.find('span', recursive=False)
-                        product_name = product_name_span.text.strip()
-                        actual_price_p_element = product_soup.find('p', {"class": "actual-price"})
-                        price_span_element = actual_price_p_element.find('span', {"class": "price-num", "id": True})
-                        price_text = price_span_element.text.strip()
-                        
-                        # Remove non-numeric characters from price_text
-                        price_numeric = re.sub(r'[^\d.]', '', price_text)  # Keeps digits and decimal point
-                        price_value = float(price_numeric)  # Convert to float for numeric representation
-                        
-                        image_filename = ""
-                        if product_image_url:
-                            image_response = requests.get(product_image_url, stream=True)
-                            if image_response.status_code == 200:
-                                image_filename = os.path.join(image_folder, f"{product_name.replace(' ', '_')}.jpg").replace('\\', '/')
-                                with open(image_filename, 'wb') as image_file:
-                                    for chunk in image_response.iter_content(1024):
-                                        image_file.write(chunk)
-                        else:
-                            # If the image URL is empty or not found, set image_filename to None or empty
-                            image_filename = None
-                        
-                        # Save product data to CSV
-                        with open(csv_filename, mode='a', newline='') as file:
-                            writer = csv.writer(file)
-                            writer.writerow([
-                                brand_name,
-                                club_type_name,
-                                product_name,
-                                price_value,
-                                options_list if options_list is not None else "NULL",
-                                image_filename
-                            ])
-                            
-                        print(f"Saved: {brand_name}::{club_type_name}: {product_name} - Price: {price_text} - Attributes: {options_list} - Image: {image_filename}")
+                    if product_listing_element is not None:
+                        div_elements = product_listing_element.find_all('div', {"class": "prices-container clearfix"})
+                        next_buttons = category_soup.find_all('a', {"class": "next cm-history cm-ajax"})
+                        for k in range(0, len(next_buttons)+1):
+                            if k == 0:
+                                product_div_elements = div_elements
+                            else:
+                                next_button_url = next_buttons[k-1]['href']
+                                next_product_page = requests.get(next_button_url)
+                                next_product_page_soup = BeautifulSoup(next_product_page.text, 'html.parser')
+                                next_product_listing_element = next_product_page_soup.find('div', {"class": "pagination-container cm-pagination-container"})
+                                product_div_elements = next_product_listing_element.find_all('div', {"class": "prices-container clearfix"})
+                            for element in product_div_elements:
+                                child_a_element = element.find('a', recursive=False)
+                                product_url = child_a_element['href']
+                                product_page = requests.get(product_url)
+                                product_soup = BeautifulSoup(product_page.text, 'html.parser')
+                                options_list = []
+                                option_div_elements = product_soup.find_all('div', {"class": "control-group product-list-field clearfix"})
+                                if option_div_elements:
+                                    for l in range(0, len(option_div_elements)):
+                                        select_element = option_div_elements[l].find('select', recursive=False)
+                                        if select_element:  # Check if the select element exists
+                                            child_option_elements = select_element.find_all('option', recursive=False)
+                                            for option_element in child_option_elements:
+                                                option_element_text = option_element.text.strip()
+                                                options_list.append(option_element_text)
+                                else:
+                                    # Set options_list to None if no matching div elements are found
+                                    options_list = None
+                                
+                                # Collect image, name, and price data
+                                image_a_element = product_soup.find('a', {"class": "cm-image-previewer"})
+                                product_image_url = image_a_element['href'] if image_a_element and image_a_element.has_attr('href') else None
 
+                                product_name_div = product_soup.find('div', {"class": "breadcrumbs clearfix"})
+
+                                # Check if product_name_div is found
+                                if product_name_div is not None:
+                                    product_name_span = product_name_div.find('span', recursive=False)
+                                    product_name = product_name_span.text.strip()
+                                    actual_price_p_element = product_soup.find('p', {"class": "actual-price"})
+                                    price_span_element = actual_price_p_element.find('span', {"class": "price-num", "id": True})
+                                    price_text = price_span_element.text.strip()
+                                    
+                                    # Remove non-numeric characters from price_text
+                                    price_numeric = re.sub(r'[^\d.]', '', price_text)  # Keeps digits and decimal point
+                                    price_value = float(price_numeric)  # Convert to float for numeric representation
+                                    
+                                    image_filename = ""
+                                    if product_image_url:
+                                        image_response = requests.get(product_image_url, stream=True)
+                                        if image_response.status_code == 200:
+                                            safe_product_name = re.sub(r'[<>:"/\\|?*]', '_', product_name)
+                                            image_filename = os.path.join(image_folder, f"{safe_product_name}.jpg").replace('\\', '/')
+                                            with open(image_filename, 'wb') as image_file:
+                                                for chunk in image_response.iter_content(1024):
+                                                    image_file.write(chunk)
+                                    else:
+                                        # If the image URL is empty or not found, set image_filename to None or empty
+                                        image_filename = None
+                                    
+                                    # Save product data to CSV
+                                    with open(csv_filename, mode='a', newline='') as file:
+                                        writer = csv.writer(file)
+                                        writer.writerow([
+                                            brand_name,
+                                            club_type_name,
+                                            product_name,
+                                            price_value,
+                                            options_list if options_list is not None else "NULL",
+                                            image_filename if image_filename is not None else "NULL"
+                                        ])
+                                    m += 1
+                                    print(f"Saved: {m}: {brand_name}")
+                                else:
+                                    print(f"Product name div not found for URL: {product_url}. Skipping this product.")
+                    else:
+                        print(f"Products are not existing.")
             except Exception as e:
                 print(f"Error processing category {i}: {e}")
     else:
